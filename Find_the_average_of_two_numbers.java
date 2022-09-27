dea3e208d1e7c7395dfee19c659f5f2b38027856
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-09-27 19:37:25 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g= new Scanner(System.in);         int a=g.nextInt();         int b=g.nextInt();         float c=(a+b)/(float)2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g= new Scanner(System.in);
+        int a=g.nextInt();
+        int b=g.nextInt();
+        float c=(a+b)/(float)2;
+        System.out.printf("%.4f",c);
+    }
+}
