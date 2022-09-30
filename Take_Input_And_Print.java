7dbdacd42253f0012532965c97cd93de70314b86
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-09-30 14:37:04 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         String a=g.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        String a=g.nextLine();
+        System.out.println(a);
+    }
+}
