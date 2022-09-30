598f1b47c8ce3d8385a35a453d303f8561e24732
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-09-30 14:41:07 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int a=g.nextInt();         int b=g.nextInt();         System.out.println(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int a=g.nextInt();
+        int b=g.nextInt();
+        System.out.println(a+b);
+    }
+}
