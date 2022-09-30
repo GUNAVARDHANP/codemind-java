db7b327b714a76f0d1b57430597177a2be9ff5af
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-09-30 14:24:48 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int a=g.nextInt();         System.out.printf("%.2f",a*a*3.14);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int a=g.nextInt();
+        System.out.printf("%.2f",a*a*3.14);
+    }
+}
