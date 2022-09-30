85ae319453073b85d0afb966bbb4a942b6a83a13
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-09-30 14:20:48 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int a=g.nextInt();         float f=((a*9)/(float)5)+32;         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int a=g.nextInt();
+        float f=((a*9)/(float)5)+32;
+        System.out.printf("%.2f",f);
+    }
+}
