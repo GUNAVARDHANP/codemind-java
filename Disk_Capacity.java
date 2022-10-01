feee5b0ab0b027e4c14a3b5be622119438d010d6
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-01 18:34:40 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int a=g.nextInt();         int b=g.nextInt();         int c=g.nextInt();         System.out.println(1024*c*b*a);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int a=g.nextInt();
+        int b=g.nextInt();
+        int c=g.nextInt();
+        System.out.println(1024*c*b*a);
+    }
+}
