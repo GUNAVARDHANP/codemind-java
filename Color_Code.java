bd52dcc8210e402b0d44f1a4e8546551af6468b8
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-01 19:08:37 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         char a=g.next().charAt(0);         if(a=='V' || a=='v'){             System.out.print("Violet");         }         else if(a=='I' || a=='i'){             System.out.print("Indigo");         }         else if(a=='B' || a=='b'){             System.out.print("Blue");         }         else if(a=='G' || a=='g'){             System.out.print("Green");         }         else if(a=='Y' || a=='y'){             System.out.print("Yellow");         }         else if(a=='O' || a=='o'){             System.out.print("Orange");         }         else if(a=='R' || a=='r'){             System.out.print("Red");         }         else{             System.out.print("-1");         }     } }

---
+++ Color_Code.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        char a=g.next().charAt(0);
+        if(a=='V' || a=='v'){
+            System.out.print("Violet");
+        }
+        else if(a=='I' || a=='i'){
+            System.out.print("Indigo");
+        }
+        else if(a=='B' || a=='b'){
+            System.out.print("Blue");
+        }
+        else if(a=='G' || a=='g'){
+            System.out.print("Green");
+        }
+        else if(a=='Y' || a=='y'){
+            System.out.print("Yellow");
+        }
+        else if(a=='O' || a=='o'){
+            System.out.print("Orange");
+        }
+        else if(a=='R' || a=='r'){
+            System.out.print("Red");
+        }
+        else{
+            System.out.print("-1");
+        }
+    }
+}
