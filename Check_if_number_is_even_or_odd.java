43a28840872a0a98affb720650742326d83ab507
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-02 13:22:30 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int a=g.nextInt();         if(a%2==0){             System.out.print("Even");         }         else{             System.out.print("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int a=g.nextInt();
+        if(a%2==0){
+            System.out.print("Even");
+        }
+        else{
+            System.out.print("Odd");
+        }
+    }
+}
