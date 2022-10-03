4d760a0818f182d4fe524d3aebd8aa57bbaf546d
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-03 20:59:05 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=g.nextInt();         }         int sum=0;         int b=g.nextInt();         int c=g.nextInt();         for(int i=0;i<n;i++){             if(a[i]>=b && a[i]<=c)             sum+=a[i];         }         System.out.println(sum);     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=g.nextInt();
+        }
+        int sum=0;
+        int b=g.nextInt();
+        int c=g.nextInt();
+        for(int i=0;i<n;i++){
+            if(a[i]>=b && a[i]<=c)
+            sum+=a[i];
+        }
+        System.out.println(sum);
+    }
+}
