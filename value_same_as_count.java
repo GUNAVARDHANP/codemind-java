162c6833b566e2e5e0657d217e962afbbdee9407
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-04 10:52:11 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt();         int[] a=new int[n];         int c,k=0,d=0;         int[] b=new int[n];         for(int i=0;i<n;i++){             a[i]=g.nextInt();             c=1;             for(int j=0;j<k;j++){                 if(a[i]==b[j]){                     c=0;                     break;                 }             }             if(c==1)             b[k++]=a[i];         }         for(int i=0;i<k;i++){             c=0;             for(int j=0;j<n;j++){                 if(a[j]==b[i])                 c+=1;             }             if(c==b[i])             d+=1;         }         System.out.println(d);     } }

---
+++ value_same_as_count.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt();
+        int[] a=new int[n];
+        int c,k=0,d=0;
+        int[] b=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=g.nextInt();
+            c=1;
+            for(int j=0;j<k;j++){
+                if(a[i]==b[j]){
+                    c=0;
+                    break;
+                }
+            }
+            if(c==1)
+            b[k++]=a[i];
+        }
+        for(int i=0;i<k;i++){
+            c=0;
+            for(int j=0;j<n;j++){
+                if(a[j]==b[i])
+                c+=1;
+            }
+            if(c==b[i])
+            d+=1;
+        }
+        System.out.println(d);
+    }
+}
