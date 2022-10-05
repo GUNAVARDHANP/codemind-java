a56b5c583b0f6f040068432229638880ef71954c
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-05 15:58:56 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt(),c=0;         int[] a=new int[n];         for(int i=0;i<n;i++)             a[i]=g.nextInt();         int k=g.nextInt();         for(int i=0;i<n;i++){             if(a[i]==k)                 c+=1;         }         System.out.println(c);     } }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt(),c=0;
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=g.nextInt();
+        int k=g.nextInt();
+        for(int i=0;i<n;i++){
+            if(a[i]==k)
+                c+=1;
+        }
+        System.out.println(c);
+    }
+}
