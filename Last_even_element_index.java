4c67225a999671214228f36b30f694b3121d75bb
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-05 15:51:33 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt(),c=0;         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=g.nextInt();             if(a[i]%2==0)                 c=i;         }         System.out.println(c);     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt(),c=0;
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=g.nextInt();
+            if(a[i]%2==0)
+                c=i;
+        }
+        System.out.println(c);
+    }
+}
