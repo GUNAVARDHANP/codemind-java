28d6397c0631901b8ec444e05b5bd05f09a9312a
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-05 16:43:31 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt();         System.out.printf("%.2f",(n-32)*5/(float)9);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt();
+        System.out.printf("%.2f",(n-32)*5/(float)9);
+    }
+}
