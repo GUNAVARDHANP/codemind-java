2c31ab66b62537eca60add6e5918d7918804861d
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-09 17:55:16 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt();         for(int i=0;i<n;i++)             System.out.println(g.nextLong()+g.nextLong());     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt();
+        for(int i=0;i<n;i++)
+            System.out.println(g.nextLong()+g.nextLong());
+    }
+}
