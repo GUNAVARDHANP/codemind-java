b1af325b009e46ed8928d7d01461ced2151aa8cd
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-12 10:52:34 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int p=g.nextInt();         int t=g.nextInt();         int r=g.nextInt();         System.out.print(p*t*r/100);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int p=g.nextInt();
+        int t=g.nextInt();
+        int r=g.nextInt();
+        System.out.print(p*t*r/100);
+    }
+}
