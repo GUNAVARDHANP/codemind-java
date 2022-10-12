c0225798610ab0e133053a84e0b2d62008b33b53
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-10-12 10:17:28 +0530

    import java.util.*; class lo{     public static void main(String[] args){         Scanner g=new Scanner(System.in);         int n=g.nextInt();         int a=g.nextInt();         for(int i=1;i<=a;i+=2)             System.out.print(n+" x "+i+" = "+n*i+" ");     } }

---
+++ Tables.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class lo{
+    public static void main(String[] args){
+        Scanner g=new Scanner(System.in);
+        int n=g.nextInt();
+        int a=g.nextInt();
+        for(int i=1;i<=a;i+=2)
+            System.out.print(n+" x "+i+" = "+n*i+"
+");
+    }
+}
